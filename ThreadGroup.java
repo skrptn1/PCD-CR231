91861b5fa7115c52f0a4e315b538e1ce7f33405d
HacPlay <ythacplaythem@gmail.com>
2025-09-29 14:13:56 +0300

    ё

--- ThreadGroup.java
+++ ThreadGroup.java
@@ -3,7 +3,20 @@ public class ThreadGroup {
         java.lang.ThreadGroup sys=
         Thread.currentThread() .getThreadGroup();
         sys.list();
-
-
+        sys.setMaxPriority(Thread.MAX_PRIORITY-2);
+        Thread current=Thread.currentThread();
+        current.setPriority(current.getPriority());
+        sys.list();
+        java.lang.ThreadGroup gn=new java.lang.ThreadGroup("GV");
+        java.lang.ThreadGroup gh=new java.lang.ThreadGroup("gn","GH"); 
+        Thread Tha=new Thread(gn,"THA");
+        Tha.setPriority(4);
+        Thread Thb=new Thread(gh,"THB");
+        Thb.setPriority(3);
+        Thread Thc=new Thread(gh,"THC");
+        Thc.setPriority(6);
+        Thread Thd=new Thread(gn,"THD");
+        Thd.setPriority(3);
+        gn.list();
     }
     }
