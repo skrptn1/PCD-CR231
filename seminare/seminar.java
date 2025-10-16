1e8cdd0bfb8ef7c052cc6ed68db723386f336259
Maxim031 <maxim.munteanu.2000@gmail.com>
2025-10-16 16:28:25 +0300

    seminaar

--- seminare/seminar.java
+++ seminare/seminar.java
@@ -50,7 +50,7 @@ class ThreadLitere extends Thread {
     }
 
     public void run() {
-        char lit[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k','l','m','n','o','p','q','r','s','t','u','v','w','x'},;
+        char lit[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k','l','m','n','o','p','q','r','s','t','u','v','w','x'};
 
         for (int i = 0; i < 24; i++) {
             if (Thread.currentThread().getName().equals("Litere")) {
