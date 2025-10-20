9880698b9f434c6c25d42e2d08be1a557c407400
Pavel Buga <pavel.buga@gmail.com>
2025-10-20 14:09:18 +0300

    lab2 adaugare interfata

--- lab2.java
+++ lab2.java
@@ -1,65 +1,18 @@
+import javax.swing.*;
+
 class Thread1 implements Runnable {
     private int from, to, step;
     private int[] tablou;
     private String name;
+    private JTextArea textArea;
 
-    public Thread1(String name, int from, int to, int step, int[] tablou) {
+    public Thread1(String name, int from, int to, int step, int[] tablou, JTextArea textArea) {
         this.name = name;
         this.from = from;
         this.to = to;
         this.step = step;
         this.tablou = tablou;
-    }
-
-    @Override
-    public void run() {
-        int suma = 0;
-        int cnt=0;
-        int i = from;
-        while (i != to) {
-            if ((tablou[i] % 2) == 0) {
-                cnt++;
-                suma+=tablou[i];
-                if(cnt==2)
-                {
-                    cnt=0;
-                    System.out.println("Thread "+this.name+" suma: "+suma);
-                    suma=0;
-                }
-            }
-            i += step;
-        }
-        if(from==0)
-        {
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-            String info = new String("Thread "+this.name+": Au efectuat lucrarea Buga Pavel si Bivol Daniel, grupa CR-231 var1.");
-            for(char j :  info.toCharArray())
-            {
-                try {
-                    System.out.print(j);
-                    Thread.sleep(100);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        }
-    }
-}
-class Thread2 implements Runnable {
-    private int from, to, step;
-    private int[] tablou;
-    private String name;
-
-    public Thread2(String name, int from, int to, int step, int[] tablou) {
-        this.name = name;
-        this.from = from;
-        this.to = to;
-        this.step = step;
-        this.tablou = tablou;
+        this.textArea = textArea;
     }
 
     @Override
@@ -73,7 +26,64 @@ class Thread2 implements Runnable {
                 suma += tablou[i];
                 if (cnt == 2) {
                     cnt = 0;
-                    System.out.println("Thread " + this.name + " suma: " + suma);
+                    String mesaj = "Thread " + this.name + " suma: " + suma;
+                    System.out.println(mesaj);
+                    SwingUtilities.invokeLater(() -> textArea.append(mesaj + "\n"));
+                    suma = 0;
+                }
+            }
+            i += step;
+        }
+        if (from == 0) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            String info = "Thread " + this.name + ": Au efectuat lucrarea Buga Pavel si Bivol Daniel, grupa CR-231 var1.";
+            for (char j : info.toCharArray()) {
+                try {
+                    System.out.print(j);
+                    SwingUtilities.invokeLater(() -> textArea.append(j + ""));
+                    Thread.sleep(50);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            SwingUtilities.invokeLater(() -> textArea.append("\n"));
+        }
+    }
+}
+
+class Thread2 implements Runnable {
+    private int from, to, step;
+    private int[] tablou;
+    private String name;
+    private JTextArea textArea;
+
+    public Thread2(String name, int from, int to, int step, int[] tablou, JTextArea textArea) {
+        this.name = name;
+        this.from = from;
+        this.to = to;
+        this.step = step;
+        this.tablou = tablou;
+        this.textArea = textArea;
+    }
+
+    @Override
+    public void run() {
+        int suma = 0;
+        int cnt = 0;
+        int i = from;
+        while (i != to) {
+            if ((tablou[i] % 2) == 0) {
+                cnt++;
+                suma += tablou[i];
+                if (cnt == 2) {
+                    cnt = 0;
+                    String mesaj = "Thread " + this.name + " suma: " + suma;
+                    System.out.println(mesaj);
+                    SwingUtilities.invokeLater(() -> textArea.append(mesaj + "\n"));
                     suma = 0;
                 }
             }
@@ -83,20 +93,38 @@ class Thread2 implements Runnable {
 }
 
 class Main {
-    static void main(String[] args) {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Interfață Text");
+        frame.setSize(600, 400);
+
+        JTextArea textArea = new JTextArea();
+        textArea.setEditable(false);
+        JScrollPane scrollPane = new JScrollPane(textArea);
+
+        frame.add(scrollPane);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
         int[] tablou = new int[100];
-
         for (int i = 0; i < 100; i++) {
-            tablou[i] = (int)(Math.random()*99)+1;
-            System.out.print(tablou[i] + " ");
+            tablou[i] = (int) (Math.random() * 99) + 1;
         }
-        System.out.println();
 
-        Thread t1 = new Thread(new Thread1("Unu", 0, tablou.length, 1, tablou));
-        Thread t2 = new Thread(new Thread1("Doi", tablou.length - 1, -1, -1, tablou));
+        StringBuilder sb = new StringBuilder("Tablou: \n");
+        for (int i = 0; i < tablou.length; i++) {
+            sb.append(tablou[i]).append(" ");
+            if(i%10==0 && i!=0){
+                sb.append("\n");
+            }
+        }
+        String tablouText = sb.toString() + "\n\n";
+        System.out.println(tablouText);
+        textArea.append(tablouText);
 
-        Thread t3 = new Thread(new Thread2("Trei", 0, tablou.length, 1, tablou));
-        Thread t4 = new Thread(new Thread2("Patru", tablou.length - 1, -1, -1, tablou));
+        Thread t1 = new Thread(new Thread1("Unu", 0, tablou.length, 1, tablou, textArea));
+        Thread t2 = new Thread(new Thread1("Doi", tablou.length - 1, -1, -1, tablou, textArea));
+        Thread t3 = new Thread(new Thread2("Trei", 0, tablou.length, 1, tablou, textArea));
+        Thread t4 = new Thread(new Thread2("Patru", tablou.length - 1, -1, -1, tablou, textArea));
 
         t1.start();
         t2.start();
@@ -104,4 +132,3 @@ class Main {
         t4.start();
     }
 }
-
