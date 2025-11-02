726aa1cef7b7d7c5a566fc7f427ef646cc938cc7
Vladic204 <vladislavburlea09@gmail.com>
2025-11-02 13:30:23 +0200

    add thread

--- lab2/Th2.java
+++ lab2/Th2.java
@@ -1,23 +1,34 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.Random;
+
+
 class Th2 extends Thread {
     int[] mas;
+    JTextArea textArea;
 
-    Th2(int[] mas) {
+    Th2(int[] mas, JTextArea textArea) {
         this.mas = mas;
+        this.textArea = textArea;
     }
 
     @Override
     public void run() {
-        System.out.println("Th2 - Condiția 2 (varianta 8):");
+        
+        SwingUtilities.invokeLater(() ->
+            textArea.append("Th2 - Condiția 2 (varianta 8):\n")
+        );
+
         int sumaProduselor = 0;
         int count = 0;
         int produs = 1;
 
-        // Parcurgere de la sfârșit spre început
+    
         for (int i = mas.length - 1; i >= 0; i--) {
-            if (mas[i] % 2 != 0) {  // număr impar
+            if (mas[i] % 2 != 0) {  
                 produs *= mas[i];
                 count++;
-                if (count == 2) {  // la fiecare 2 impare
+                if (count == 2) {  
                     sumaProduselor += produs;
                     produs = 1;
                     count = 0;
@@ -25,6 +36,60 @@ class Th2 extends Thread {
             }
         }
 
-        System.out.println("Suma produselor numerelor impare doua cate doua (de la ultimul): " + sumaProduselor);
+        int rezultatFinal = sumaProduselor;
+
+        
+        SwingUtilities.invokeLater(() ->
+            textArea.append("Suma produselor numerelor impare două câte două (de la ultimul): " + rezultatFinal + "\n\n")
+        );
+    }
+}
+
+public class MainSwing {
+    public static void main(String[] args) {
+        
+        JFrame frame = new JFrame("Lucrare cu Threads și Swing - Condiția 2 (varianta 8)");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(600, 400);
+        frame.setLocationRelativeTo(null);
+
+        
+        JTextArea textArea = new JTextArea();
+        textArea.setEditable(false);
+        textArea.setFont(new Font("Consolas", Font.PLAIN, 16));
+        JScrollPane scrollPane = new JScrollPane(textArea);
+
+        frame.add(scrollPane);
+        frame.setVisible(true);
+
+        
+        int[] mas = new int[100];
+        Random rnd = new Random();
+        for (int i = 0; i < mas.length; i++) {
+            mas[i] = rnd.nextInt(100) + 1;
+        }
+
+        
+        Th2 t2 = new Th2(mas, textArea);
+        t2.start();
+
+        
+        new Thread(() -> {
+            try {
+                t2.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            String info = "Lucrarea realizată de: Popescu Ion și Ionescu Maria";
+            for (char c : info.toCharArray()) {
+                SwingUtilities.invokeLater(() -> textArea.append(String.valueOf(c)));
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
     }
 }
