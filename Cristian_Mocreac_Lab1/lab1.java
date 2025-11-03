02c856880a904e597738761e2c82f6e6f8862d03
CristiMocreac <mocreaccristian@gmail.com>
2025-11-03 14:23:25 +0200

    lab1+

---
+++ Cristian_Mocreac_Lab1/lab1.java
@@ -0,0 +1,70 @@
+import java.util.Random;
+
+public class lab1 {
+    public static void main(String[] args) throws InterruptedException {
+        int[] mas = generateArray(100);
+
+        Runnable r1 = new Conditie1Task(mas);
+        Runnable r2 = new Conditie1Task(mas);
+
+        Thread th1 = new Thread(r1, "Th1");
+        Thread th2 = new Thread(r2, "Th2");
+
+        th1.start();
+        th2.start();
+
+        th1.join();
+        th2.join();
+
+        String text = "Studentii care au efectuat lucrarea de laborator: Popescu Ion, "
+                + "Ionescu Maria, Gherman Mihai.";
+        printSlow(text, 100);
+    }
+
+    private static int[] generateArray(int n) {
+        int[] a = new int[n];
+        Random rand = new Random();
+        for (int i = 0; i < n; i++) {
+            a[i] = rand.nextInt(100) + 1; 
+        }
+        return a;
+    }
+
+    private static void printSlow(String s, int delayMs) {
+        for (int i = 0; i < s.length(); i++) {
+            System.out.print(s.charAt(i));
+            try {
+                Thread.sleep(delayMs);
+            } catch (InterruptedException e) {
+            }
+        }
+        System.out.println();
+    }
+}
+
+// clasa de lucru pentru Conditia 1
+class Conditie1Task implements Runnable {
+    private final int[] mas;
+
+    public Conditie1Task(int[] mas) {
+        this.mas = mas;
+    }
+
+    @Override
+    public void run() {
+        int suma = calculeazaSumaCond1(mas);
+        System.out.println(Thread.currentThread().getName()
+                + " -> Suma produselor (de la inceput, pozitii pare): " + suma);
+    }
+
+    private int calculeazaSumaCond1(int[] a) {
+        int suma = 0;
+        // perechi (0,2), (4,6), (8,10) ... adica index par, urmatorul index par
+        for (int i = 0; i + 2 < a.length; i += 4) {
+            int x = a[i];
+            int y = a[i + 2];
+            suma += x * y;
+        }
+        return suma;
+    }
+}
