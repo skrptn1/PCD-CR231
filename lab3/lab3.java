d9b4e9c311f9dbd87a86a7e23accc19dd05686db
Maxim031 <maxim.munteanu.2000@gmail.com>
2025-11-03 14:02:29 +0200

    lab 3 schimbari

--- lab3/lab3.java
+++ lab3/lab3.java
@@ -2,20 +2,17 @@ package lab3;
 public class lab3 {
     public static void main(String[] args) {
 
-        // 1. Generăm 2112 numere aleatoare
         int[] Tablou = new int[2112];
         for (int i = 0; i < Tablou.length; i++) {
             Tablou[i] = (int) (Math.random() * 99);
         }
 
-        // 2. Afișăm primele 100 pentru verificare
         System.out.println("TABLOU (primele 100 numere):");
         for (int i = 0; i < 100; i++) {
             System.out.print(Tablou[i] + " ");
         }
         System.out.println("\n");
 
-        // 3. Cream thread-urile cu intervalele corecte
         Counter1 cnt1 = new Counter1(0, 399, 1, Tablou);
         Counter1 cnt2 = new Counter1(400, 798, 1, Tablou);
         Counter1 cnt3 = new Counter1(1784, 2111, 1, Tablou); // de la sfârșit
@@ -26,13 +23,12 @@ public class lab3 {
         cnt3.setName("MAXIM-Trei");
         cnt4.setName("MAXIM-Patru");
 
-        // 4. Pornim thread-urile
         cnt1.start();
         cnt2.start();
         cnt3.start();
         cnt4.start();
 
-        // 5. Așteptăm să termine
+
         try {
             cnt1.join();
             cnt2.join();
@@ -42,13 +38,10 @@ public class lab3 {
             e.printStackTrace();
         }
 
-        // 6. Thread pentru afișarea numelui
         NameThread nameThread = new NameThread("Ungureanu Vlad, Munteanu Maxim - Grupul 3");
         nameThread.start();
     }
 }
-
-// Clasa pentru numărarea numerelor impare
 class Counter1 extends Thread {
     int from, to, step;
     int[] Tablou;
@@ -62,11 +55,11 @@ class Counter1 extends Thread {
 
     @Override
     public void run() {
-        int count = 0;   // numără câte impare am procesat
-        int suma1 = 0;   // primele 2 impare
-        int suma2 = 0;   // următoarele 2 impare
+        int count = 0; 
+        int suma1 = 0; 
+        int suma2 = 0;
 
-        if (from <= to) { // parcurgere crescătoare
+        if (from <= to) { 
             for (int i = from; i <= to; i += step) {
                 if (Tablou[i] % 2 != 0) {
                     if (count < 2) suma1 += Tablou[i];
@@ -86,7 +79,7 @@ class Counter1 extends Thread {
                     }
                 }
             }
-        } else { // parcurgere descrescătoare
+        } else { 
             for (int i = from; i >= to; i -= step) {
                 if (Tablou[i] % 2 != 0) {
                     if (count < 2) suma1 += Tablou[i];
@@ -110,7 +103,7 @@ class Counter1 extends Thread {
     }
 }
 
-// Clasa pentru afișarea numelui cu delay
+
 class NameThread extends Thread {
     String name;
 
