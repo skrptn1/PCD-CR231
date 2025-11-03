768e85e09220220522adaa70fb316060e4993268
senseivictor <43610277+senseivictor@users.noreply.github.com>
2025-11-03 12:30:26 +0200

    started working on lab3

--- lab3/Th1.java
+++ lab3/Th1.java
@@ -1,38 +1,5 @@
 package lab3;
 
-class Array {
-
-    int[] a;
-    int currentIndex = 0;
-    App app;
-
-    public Array(int size, App app) {
-        a = new int[size];
-        this.app = app;
-    }
-
-    public void add(int value) {
-        a[currentIndex] = value;
-        if (currentIndex < a.length) {
-            currentIndex++;
-        }
-    }
-
-    public void print() {
-        for (int elem : a) {
-            if (elem != 0) {
-                app.appendText(elem + " ");
-                // System.out.print(elem + " ");
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
-}
-
 class Th1 implements Runnable {
 
     String name = "Th1";
@@ -51,7 +18,6 @@ class Th1 implements Runnable {
     }
 
     public void run() {
-        Array sumArray = new Array(a.length / 2, app);
         int s = 0;
         int counter = 0;
         app.appendText("\n");
@@ -59,12 +25,11 @@ class Th1 implements Runnable {
         for (int i = 0; i < a.length; i++) {
             if (a[i] % 2 == 0) {
                 if (counter <= 1) {
-                    s = s + i;
+                    s = s + a[i];
                     counter++;
                     if (counter == 2) {
                         app.appendText("Victor " + name + " " + s + "\n");
-                        System.out.println("Victor " + name + " " + s);
-                        sumArray.add(s);
+                        // System.out.println("Victor " + name + " " + s);
                         s = 0;
                         counter = 0;
                     }
