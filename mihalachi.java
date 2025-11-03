f009b4a9d58cc900ed326095c1980a479fb0a49a
EpiCnNeS2 <cristianmihalachi23@gmail.com>
2025-11-03 14:24:06 +0200

    lab2

--- mihalachi.java
+++ mihalachi.java
@@ -1,41 +1,46 @@
 public class mihalachi {
-public static void main(String[] args) {
-    ThreadGroup sys =
-        Thread.currentThread().getThreadGroup();
-    sys.list();
-    Thread curr = Thread.currentThread();
-    curr.setPriority(curr.getPriority() - 2);
-sys.list();
-Thread Th1 = new Thread(sys,"Th1");
-    Th1.start();
-Thread Th2 = new Thread(sys,"Th2");
-    Th2.start();
-    sys.list();
-ThreadGroup g = new ThreadGroup("GV");
-Thread ThA = new Thread(g, "ThA");
-    ThA.start();
-    g.list();
-ThreadGroup o = new ThreadGroup("GO");
-ThreadGroup z = new ThreadGroup(o, "GZ");
-Thread Tha = new Thread(z, "Tha");
-    Tha.setPriority(1);
-    Tha.start();
-Thread Thb = new Thread(z, "Thb");
-    Thb.start();
-Thread Thc = new Thread(z, "Thc");
-    Thc.start();
-Thread Thd = new Thread(z, "Thd");
-    Thd.setPriority(7);
-    Thd.start();
-    z.list();
-ThreadGroup f = new ThreadGroup("GF");
-Thread Th1gf = new Thread(f, "Th1");
-    Th1gf.setPriority(5);
-    Th1gf.start();
-Thread Th2gf = new Thread(f, "Th2");
-    Th2gf.start();
-Thread Th3 = new Thread(f, "Th3");
-    Th3.setPriority(9);
-    Th3.start();
-    f.list();
-}}
+    public static void main(String[] args) throws InterruptedException {
+        ThreadGroup systemGroup = Thread.currentThread().getThreadGroup();
+        systemGroup.list();
+        
+        Thread currentThread = Thread.currentThread();
+        int newPriority = currentThread.getPriority() - 2;
+        currentThread.setPriority(newPriority);
+		Thread T1 = new Thread("Th1");
+		T1.start();
+        Thread T2 = new Thread("Th2");
+        T2.start();
+        systemGroup.list();
+
+        ThreadGroup GV = new ThreadGroup("GV");
+        Thread ThA = new Thread(GV, "ThA");
+        ThA.start();
+        GV.list();
+        
+        ThreadGroup GO = new ThreadGroup("GO");
+        ThreadGroup GZ = new ThreadGroup(GO, "GZ");
+        
+        Thread Tha = new Thread(GZ, "Tha");
+        Tha.setPriority(1);
+        Tha.start();
+        Thread Thb = new Thread(GZ, "Thb");
+        Thb.start();
+        Thread Thc = new Thread(GZ, "Thc");
+        Thc.start();
+        Thread Thd = new Thread(GZ, "Thd");
+        Thd.setPriority(7);
+        Thd.start();
+        GZ.list();
+        
+        ThreadGroup GF = new ThreadGroup("GF");
+        Thread Th1gf = new Thread(GF, "Th1");
+        Th1gf.setPriority(5);
+        Th1gf.start();
+        Thread Th2gf = new Thread(GF, "Th2");
+        Th2gf.start();
+        Thread Th3 = new Thread(GF, "Th3");
+        Th3.setPriority(9);
+        Th3.start();
+        GF.list();
+    }
+}
