77afb900f14550afa2ef84f81f23b9a3fe590854
Victor <victorpavalache2004@gmail.com>
2025-11-24 15:00:16 +0200

    modificari ca in exemplu

--- lab4/Main.java
+++ lab4/Main.java
@@ -1,87 +1,39 @@
 package lab4;
 
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
 
-// Clasa Resursa Comuna (Depozitul)
 class Store {
-
-    // Variabilele preluate din clasa Main (public static final)
     private final int CAPACITY = Main.D;
-    private final int MAX_CONSUMPTION = Main.MAX_CONSUMPTION;
+    ArrayList<Integer> stockList = new ArrayList<Integer>();
 
-    private int totalConsumed = 0;
-    private List<Integer> stockList = new ArrayList<>();
-
-    public synchronized boolean isFinished() {
-        return totalConsumed >= MAX_CONSUMPTION;
-    }
-
-    // Metoda get() - pentru Consumator
-    public synchronized int get(String consumerName) throws InterruptedException {
-        // Iesire rapida daca s-a terminat consumul total (Y*Z)
-        if (isFinished()) {
-            notifyAll();
-            return -1;
+    public synchronized void get(String consumerName) {
+        while (stockList.size() < 1) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+            }
         }
-
-        // Asteapta pana cand depozitul nu este gol SAU s-a terminat consumul
-        while (stockList.isEmpty() && !isFinished()) {
-            System.out.println("--- Depozitul este gol. " + consumerName + " asteapta. ---");
-            wait();
-        }
-
-        // Re-verifica dupa ce a fost trezit
-        if (isFinished()) {
-            notifyAll();
-            return -1;
-        }
-
-        // Extrage ultimul element
-        int value = stockList.remove(stockList.size() - 1);
-        totalConsumed++;
-
-        System.out.println(consumerName + " a luat din depozit: " + value);
-
+        System.out.println(consumerName + " a luat din depozit: " +
+                stockList.get(stockList.size() - 1));
+        stockList.remove(stockList.size() - 1);
         printStockStatus();
-
-        // Notifica Producatorii (depozitul ar putea avea loc acum) si alti Consumatori
         notifyAll();
-
-        return value;
     }
 
-    // Metoda put() - pentru Producator
-    public synchronized void put(String producerName, int a, int b) throws InterruptedException {
-        // Nu mai produce daca obiectivul de consum a fost atins
-        if (isFinished()) {
-            notifyAll();
-            return;
-        }
+    public synchronized void put(String producerName, int a, int b) {
 
-        // Asteapta pana cand depozitul nu este plin (se verifica daca pot fi adaugate
-        // cele F obiecte)
-        while (stockList.size() + Main.F > CAPACITY) {
+        while (stockList.size() + Main.F >= CAPACITY) {
             System.out.println("+++ Depozitul e plin (" + CAPACITY + "). " + producerName + " asteapta. +++");
-            wait();
+            try {
+                wait();
+            } catch (Exception e) {
+                // TODO: handle exception
+            }
         }
-
-        // Re-verifica dupa ce a fost trezit
-        if (isFinished()) {
-            notifyAll();
-            return;
-        }
-
-        // Plaseaza Main.F obiecte (F=2)
         stockList.add(a);
         stockList.add(b);
-
         System.out.println(producerName + " a pus in depozit doua numere: " + a + ", " + b);
-
         printStockStatus();
-
-        // Notifica Consumatorii blocati (ca s-au adaugat elemente)
         notifyAll();
     }
 
@@ -93,17 +45,13 @@ class Store {
             for (int number : stockList) {
                 System.out.print(number + " ");
             }
-            System.out.println("\n(Consumate total: " + totalConsumed + ")");
+            System.out.println();
         }
     }
 }
 
-// Clasa Producator (X=3)
 class Producer extends Thread {
     private Store s;
-    private Random random = new Random();
-    // Numere impare (Tip Obiecte)
-    private final int[] ODD_NUMBERS = new int[] { 1, 3, 5, 7, 9, 11, 13, 15, 17, 19 };
 
     public Producer(Store s) {
         this.s = s;
@@ -111,28 +59,17 @@ class Producer extends Thread {
 
     @Override
     public void run() {
-        try {
-            while (!s.isFinished()) {
-                // Genereaza Main.F (2) numere impare
-                int num1 = ODD_NUMBERS[random.nextInt(ODD_NUMBERS.length)];
-                int num2 = ODD_NUMBERS[random.nextInt(ODD_NUMBERS.length)];
-
-                s.put(getName(), num1, num2);
-
-                Thread.sleep(random.nextInt(100) + 50);
-            }
-        } catch (InterruptedException e) {
-            // Se iese din bucla la intrerupere (la finalul programului)
+        int[] impare = new int[] { 1, 3, 5, 7, 9, 11, 13, 15, 17, 19 };
+        while (true) {
+            s.put(getName(), impare[(int) (Math.random() * 9)],
+                    impare[(int) (Math.random() * 9)]);
         }
-        System.out.println(getName() + " a finalizat productia.");
     }
 }
 
-// Clasa Consumator (Y=4)
 class Consumer extends Thread {
     private Store s;
     private final int CONSUME_COUNT = Main.Z; // Z = 2 (obiecte per consumator)
-    private int consumed = 0;
 
     public Consumer(Store s) {
         this.s = s;
@@ -140,23 +77,12 @@ class Consumer extends Thread {
 
     @Override
     public void run() {
-        try {
-            while (consumed < CONSUME_COUNT) {
-                int value = s.get(getName());
 
-                // Verifica semnalul de terminare (-1)
-                if (value == -1) {
-                    break;
-                }
-
-                consumed++;
-
-                Thread.sleep(new Random().nextInt(150) + 50);
-            }
-        } catch (InterruptedException e) {
-            // Se iese din bucla la intrerupere
+        for (int i = 0; i < CONSUME_COUNT; i++) {
+            s.get(getName());
         }
-        System.out.println(getName() + " a luat " + consumed + " numere. Thread-ul a finalizat.");
+
+        System.out.println(getName() + " a luat " + CONSUME_COUNT + " numere. Thread-ul a finalizat.");
     }
 }
 
@@ -201,18 +127,8 @@ public class Main {
         c2.start();
         c3.start();
         c4.start();
-
-        // Asteapta finalizarea Consumatorilor
-        c1.join();
-        c2.join();
-        c3.join();
-        c4.join();
-
-        // Opreste Producatorii blocati (folosind .interrupt())
-        p1.interrupt();
-        p2.interrupt();
-        p3.interrupt();
-
+        while (c1.isAlive() || c2.isAlive() || c3.isAlive() || c4.isAlive()) {
+        }
         System.out.println("\nToate thread-urile au finalizat.");
     }
 }
