3058b887f59c35742ff4150532df695a9a3bdd66
HacPlay <ythacplaythem@gmail.com>
2025-12-08 13:17:23 +0200

    lab4 final

--- lab4.java
+++ lab4.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 
 public class lab4 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         Depozit depozit = new Depozit();
 
@@ -19,6 +19,8 @@ public class lab4 {
         p3.start();
         p4.start();
 
+        Thread.sleep(500);
+
         c1.start();
         c2.start();
         c3.start();
@@ -26,36 +28,59 @@ public class lab4 {
 }
 
 class Depozit {
-    char[] buffer = new char[10];
-    int count = 0;
+    private char[] buffer = new char[10];
+    private int count = 0;
+    volatile boolean inchis = false;
 
-    synchronized void produce(char c) {
-        while (count == buffer.length) {
-            System.out.println("[DEPOZIT PLIN] Producatorul asteapta...");
-            try { wait(); } catch (InterruptedException e) {}
+    synchronized void produce(char c1, char c2) throws InterruptedException {
+        // STOP producatori cand depozitul e plin (10)
+        while (count == buffer.length && !inchis) {
+            System.out.println("[DEPOZIT PLIN] Productia este oprita");
+            wait();
         }
-        buffer[count] = c;
-        count++;
+        if (inchis) return;
+
+        // garantam ca exista loc pentru 2
+        while (count + 2 > buffer.length && !inchis) {
+            wait();
+        }
+        if (inchis) return;
+
+        buffer[count++] = c1;
+        buffer[count++] = c2;
+
         notifyAll();
     }
 
-    synchronized char consume() {
-        while (count == 0) {
-            System.out.println("[DEPOZIT GOL] Consumatorul asteapta...");
-            try { wait(); } catch (InterruptedException e) {}
+    synchronized char consume() throws InterruptedException {
+        while (count == 0 && !inchis) {
+            wait();
         }
-        char c = buffer[count - 1];
-        count--;
-        notifyAll();
+
+        if (count == 0 && inchis)
+            return 0;
+
+        char c = buffer[--count];
+
+        // DACA s-au consumat cel putin 2 (count <= 8), producatorii pot relua
+        if (count <= 8) {
+            notifyAll();
+        }
+
         return c;
+    }
+
+    synchronized void inchide() {
+        inchis = true;
+        notifyAll();
     }
 }
 
 class Producator extends Thread {
-    Depozit depozit;
-    int id;
-    Random r = new Random();
-    char[] vocale = {'A', 'E', 'I', 'O', 'U'};
+    private Depozit depozit;
+    private int id;
+    private Random r = new Random();
+    private char[] vocale = {'A', 'E', 'I', 'O', 'U'};
 
     public Producator(Depozit d, int id) {
         this.depozit = d;
@@ -65,12 +90,16 @@ class Producator extends Thread {
     @Override
     public void run() {
         try {
-            while (Consumator.terminati < 3) {
-                for (int i = 0; i < 2; i++) {
-                    char v = vocale[r.nextInt(vocale.length)];
-                    depozit.produce(v);
-                    System.out.println("Producator " + id + " a produs " + v);
+            while (!depozit.inchis) {
+                char v1 = vocale[r.nextInt(vocale.length)];
+                char v2 = vocale[r.nextInt(vocale.length)];
+
+                depozit.produce(v1, v2);
+
+                synchronized (System.out) {
+                    System.out.println("Producator " + id + " a produs: " + v1 + " " + v2);
                 }
+
                 sleep(200);
             }
         } catch (InterruptedException e) {}
@@ -78,10 +107,10 @@ class Producator extends Thread {
 }
 
 class Consumator extends Thread {
-    Depozit depozit;
-    int id;
-    int necesita = 3;
-    static int terminati = 0;
+    private Depozit depozit;
+    private int id;
+    private int necesita = 3;
+    private static int terminati = 0;
 
     public Consumator(Depozit d, int id) {
         this.depozit = d;
@@ -93,14 +122,24 @@ class Consumator extends Thread {
         try {
             while (necesita > 0) {
                 char v = depozit.consume();
-                System.out.println("Consumator " + id + " a consumat " + v);
+                if (v == 0) break;
+
+                synchronized (System.out) {
+                    System.out.println("Consumator " + id + " a consumat: " + v);
+                }
+
                 necesita--;
                 sleep(300);
             }
-            System.out.println("Consumator " + id + " a terminat cele 3 obiecte.");
 
             synchronized (Consumator.class) {
                 terminati++;
+                if (terminati == 3) {
+                    depozit.inchide();
+                    synchronized (System.out) {
+                        System.out.println("=== TOTI CONSUMATORII AU TERMINAT ===");
+                    }
+                }
             }
 
         } catch (InterruptedException e) {}
