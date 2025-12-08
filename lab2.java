a05bbde270176cfe758c9ba43355d0b8fa08bbb8
gogi0372 <victor.turcan@iis.utm.md>
2025-12-08 15:15:27 +0300

    Refactor lab2.java to replace Runnable threads with extended Thread classes and improve array initialization

--- lab2.java
+++ lab2.java
@@ -1,134 +1,137 @@
-import javax.swing.*;
-
-class Thread1 implements Runnable {
-    private int from, to, step;
-    private int[] tablou;
-    private String name;
-    private JTextArea textArea;
-
-    public Thread1(String name, int from, int to, int step, int[] tablou, JTextArea textArea) {
-        this.name = name;
-        this.from = from;
-        this.to = to;
-        this.step = step;
-        this.tablou = tablou;
-        this.textArea = textArea;
-    }
-
-    @Override
-    public void run() {
-        int suma = 0;
-        int cnt = 0;
-        int i = from;
-        while (i != to) {
-            if ((tablou[i] % 2) == 0) {
-                cnt++;
-                suma += tablou[i];
-                if (cnt == 2) {
-                    cnt = 0;
-                    String mesaj = "Thread " + this.name + " suma: " + suma;
-                    System.out.println(mesaj);
-                    SwingUtilities.invokeLater(() -> textArea.append(mesaj + "\n"));
-                    suma = 0;
-                }
-            }
-            i += step;
-        }
-        if (from == 0) {
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-            String info = "Thread " + this.name + ": Au efectuat lucrarea Buga Pavel si Bivol Daniel, \ngrupa CR-231 var1.";
-            for (char j : info.toCharArray()) {
-                try {
-                    System.out.print(j);
-                    SwingUtilities.invokeLater(() -> textArea.append(j + ""));
-                    Thread.sleep(50);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-            SwingUtilities.invokeLater(() -> textArea.append("\n"));
-        }
-    }
-}
-
-class Thread2 implements Runnable {
-    private int from, to, step;
-    private int[] tablou;
-    private String name;
-    private JTextArea textArea;
-
-    public Thread2(String name, int from, int to, int step, int[] tablou, JTextArea textArea) {
-        this.name = name;
-        this.from = from;
-        this.to = to;
-        this.step = step;
-        this.tablou = tablou;
-        this.textArea = textArea;
-    }
-
-    @Override
-    public void run() {
-        int suma = 0;
-        int cnt = 0;
-        int i = from;
-        while (i != to) {
-            if ((tablou[i] % 2) == 0) {
-                cnt++;
-                suma += tablou[i];
-                if (cnt == 2) {
-                    cnt = 0;
-                    String mesaj = "Thread " + this.name + " suma: " + suma;
-                    System.out.println(mesaj);
-                    SwingUtilities.invokeLater(() -> textArea.append(mesaj + "\n"));
-                    suma = 0;
-                }
-            }
-            i += step;
-        }
-    }
-}
-
-class Main {
+public class lab2 {
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Interfață");
-        frame.setSize(600, 400);
 
-        JTextArea textArea = new JTextArea();
-        textArea.setEditable(false);
-        JScrollPane scrollPane = new JScrollPane(textArea);
+        int Tablou[] = new int[101];
 
-        frame.add(scrollPane);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
-
-        int[] tablou = new int[100];
+        System.out.println("Tablou generat:");
         for (int i = 0; i < 100; i++) {
-            tablou[i] = (int) (Math.random() * 99) + 1;
+            Tablou[i] = (int) (Math.random() * 99);
+            System.out.print(" " + Tablou[i]);
         }
+        System.out.println("\n");
 
-        StringBuilder sb = new StringBuilder("Tablou: \n");
-        for (int i = 0; i < tablou.length; i++) {
-            sb.append(tablou[i]).append(" ");
-            if(i%30==0 && i!=0){
-                sb.append("\n");
+        // Threads
+        CounterFirst cntStart = new CounterFirst(0, 99, 1, Tablou);
+        cntStart.setName("Start-Impare");
+
+        CounterLast cntEnd = new CounterLast(99, 0, 1, Tablou);
+        cntEnd.setName("End-Impare");
+
+        NameThread nameThread =
+                new NameThread("Cojocari Gabriel, Turcan Victor - Grupa 6");
+
+        cntStart.start();
+        cntEnd.start();
+
+        try {
+            cntStart.join();
+            cntEnd.join();
+        } catch (Exception e) {}
+
+        nameThread.start();
+    }
+}
+
+
+// ------------------------------------------------------------------
+// THREAD 1: De la primul element → ultimul
+// ------------------------------------------------------------------
+class CounterFirst extends Thread {
+    int from, to, step;
+    int[] Tablou;
+
+    public CounterFirst(int from, int to, int step, int[] Tablou) {
+        this.from = from; this.to = to;
+        this.step = step; this.Tablou = Tablou;
+    }
+
+    @Override
+    public void run() {
+
+        int sumaFinala = 0;
+        int counter = 0;
+        int produs = 1;
+
+        for (int i = from; i <= to; i += step) {
+
+            if (i % 2 == 1) { // doar poziții impare
+
+                produs *= Tablou[i];
+                counter++;
+
+                if (counter == 2) {
+                    System.out.println(Thread.currentThread().getName() +
+                            " Produs (start): " + produs);
+                    sumaFinala += produs;
+                    produs = 1;
+                    counter = 0;
+                }
             }
         }
-        String tablouText = sb.toString() + "\n\n";
-        System.out.println(tablouText);
-        textArea.append(tablouText);
 
-        Thread t1 = new Thread(new Thread1("Unu", 0, tablou.length, 1, tablou, textArea));
-        Thread t2 = new Thread(new Thread1("Doi", tablou.length - 1, -1, -1, tablou, textArea));
-        Thread t3 = new Thread(new Thread2("Trei", 0, tablou.length, 1, tablou, textArea));
-        Thread t4 = new Thread(new Thread2("Patru", tablou.length - 1, -1, -1, tablou, textArea));
+        System.out.println(Thread.currentThread().getName() +
+                " Suma TOTALĂ (start): " + sumaFinala);
+    }
+}
 
-        t1.start();
-        t2.start();
-        t3.start();
-        t4.start();
+
+// ------------------------------------------------------------------
+// THREAD 2: De la ultimul element → primul
+// ------------------------------------------------------------------
+class CounterLast extends Thread {
+    int from, to, step;
+    int[] Tablou;
+
+    public CounterLast(int from, int to, int step, int[] Tablou) {
+        this.from = from; this.to = to;
+        this.step = step; this.Tablou = Tablou;
+    }
+
+    @Override
+    public void run() {
+
+        int sumaFinala = 0;
+        int counter = 0;
+        int produs = 1;
+
+        for (int i = from; i >= to; i -= step) {
+
+            if (i % 2 == 1) { // doar poziții impare
+
+                produs *= Tablou[i];
+                counter++;
+
+                if (counter == 2) {
+                    System.out.println(Thread.currentThread().getName() +
+                            " Produs (end): " + produs);
+                    sumaFinala += produs;
+                    produs = 1;
+                    counter = 0;
+                }
+            }
+        }
+
+        System.out.println(Thread.currentThread().getName() +
+                " Suma TOTALĂ (end): " + sumaFinala);
+    }
+}
+
+
+// ------------------------------------------------------------------
+// THREAD afișare nume
+// ------------------------------------------------------------------
+class NameThread extends Thread {
+    String name;
+
+    public NameThread(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < name.length(); i++) {
+            System.out.print(name.charAt(i));
+            try { Thread.sleep(100); } catch (InterruptedException e) {}
+        }
     }
 }
